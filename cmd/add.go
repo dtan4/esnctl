8075e3d3d6abec092ad6c1187915b70a7e5537a3
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-07 18:29:18 +0900

    Increase instances by add command

--- cmd/add.go
+++ cmd/add.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-	"time"
+	"strconv"
 
+	"github.com/dtan4/esnctl/aws"
 	"github.com/dtan4/esnctl/es"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -13,16 +13,22 @@ import (
 var addCmd = &cobra.Command{
 	SilenceErrors: true,
 	SilenceUsage:  true,
-	Use:           "add",
+	Use:           "add <cluster URL> <AutoScaling Group> <Number to increase>",
 	Short:         "Add Elasticsearch node",
 	RunE:          doAdd,
 }
 
 func doAdd(cmd *cobra.Command, args []string) error {
-	if len(args) != 1 {
-		return errors.New("cluster URL must be specified")
+	if len(args) != 3 {
+		return errors.New("cluster URL, AutoScaling Group and the number to increase must be specified")
 	}
 	clusterURL := args[0]
+	autoScalingGroup := args[1]
+
+	delta, err := strconv.Atoi(args[2])
+	if err != nil {
+		return errors.Wrapf(err, "invalid number to increase %q", args[2])
+	}
 
 	client, err := es.New(clusterURL)
 	if err != nil {
@@ -34,9 +40,10 @@ func doAdd(cmd *cobra.Command, args []string) error {
 	}
 	defer client.EnableReallocation()
 
-	fmt.Println("TODO: Add node here")
-
-	time.Sleep(10 * time.Second)
+	_, err = aws.AutoScaling.IncreaseInstances(autoScalingGroup, delta)
+	if err != nil {
+		return errors.Wrap(err, "failed to increase instance")
+	}
 
 	return nil
 }
