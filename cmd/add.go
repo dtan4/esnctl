b21008554a77b6108ced27d2ef8307fd6fc06077
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-07 15:11:27 +0900

    Disable and enable reallocation at add command

--- cmd/add.go
+++ cmd/add.go
@@ -1,41 +1,46 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-
 package cmd
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/dtan4/esnctl/es"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	SilenceErrors: true,
+	SilenceUsage:  true,
+	Use:           "add",
+	Short:         "Add Elasticsearch node",
+	RunE:          doAdd,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("add called")
-	},
+func doAdd(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("cluster URL must be specified")
+	}
+	clusterURL := args[0]
+
+	client, err := es.New(clusterURL)
+	if err != nil {
+		return errors.Wrap(err, "failed to create Elasitcsearch API client")
+	}
+
+	if err := client.DisableReallocation(); err != nil {
+		return errors.Wrap(err, "failed to disable reallocation")
+	}
+	defer client.EnableReallocation()
+
+	fmt.Println("TODO: Add node here")
+
+	time.Sleep(10 * time.Second)
+
+	return nil
 }
 
 func init() {
 	RootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
