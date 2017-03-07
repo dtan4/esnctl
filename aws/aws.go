e09b09a6d9a8f51c20fdd924e195cad9102afd61
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-07 18:13:18 +0900

    Craete AWS client manager

---
+++ aws/aws.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	autoscalingapi "github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/dtan4/esnctl/aws/autoscaling"
+	"github.com/pkg/errors"
+)
+
+var (
+	// AutoScaling represents AutoScaling service client
+	AutoScaling *autoscaling.Client
+)
+
+// Initialize creates AWS service client objects
+func Initialize(region string) error {
+	var (
+		sess *session.Session
+		err  error
+	)
+
+	if region == "" {
+		sess, err = session.NewSession()
+		if err != nil {
+			return errors.Wrap(err, "Failed to create new AWS session.")
+		}
+	} else {
+		sess, err = session.NewSession(&aws.Config{Region: aws.String(region)})
+		if err != nil {
+			return errors.Wrap(err, "Failed to create new AWS session.")
+		}
+	}
+
+	AutoScaling = autoscaling.New(autoscalingapi.New(sess))
+
+	return nil
+}
